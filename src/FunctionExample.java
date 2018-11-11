cb1785c63ae613d0c943bcb1e58d9953eea0d485
Catalin.Florea <florea.catalin2@gmail.com>
2018-11-11 15:37:23 +0200

    Added Functional Example

--- src/FunctionExample.java
+++ src/FunctionExample.java
@@ -1,4 +1,20 @@
+import java.util.function.Function;
 
 public class FunctionExample {
+	public static void main(String[] args) {
 
+		Function<Integer, Integer> adderFunction = x -> {
+			if (x < 0)
+				return x + 2;
+			else
+				return x + 1;
+		};
+
+		System.out.println(applyAdder(10, adderFunction));
+
+	}
+
+	private static Integer applyAdder(Integer parameter, Function<Integer, Integer> fn) {
+		return fn.apply(parameter);
+	}
 }
